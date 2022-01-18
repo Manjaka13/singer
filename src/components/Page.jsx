20edea62688ac5e6430fc7081d2b3cd26e48a360
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-01-18 06:06:20 +0100

    add cover component

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Menu from "./Menu";
 import Copyright from "./Copyright";
+import Cover from "./Cover";
 
 /*
 	Page layout
@@ -16,6 +17,7 @@ const Page = ({ children }) => {
 			<Navbar atTrigger={toggleMenu} />
 			<Menu opened={menuOpened} atClose={toggleMenu} />
 			<div className="page w-100 o-scroll">
+				<Cover />
 				{children}
 				<div className="ctn">
 					<p className="prg ctn-20-0">
