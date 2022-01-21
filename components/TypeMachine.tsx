311bf81ec36882bdd7a33c89a5d2d961ed3ee257
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-01-21 16:29:47 +0100

    fix TypeMachine css

--- components/TypeMachine.tsx
+++ components/TypeMachine.tsx
@@ -38,51 +38,48 @@ const listTypes: Array<ITypeMachineItem> = [
 	},
 ];
 
-const TypeMachine = (): JSX.Element => {
-	const mappedType: Array<JSX.Element> = listTypes.map(
-		(item: ITypeMachineItem) => (
-			<li
-				className="item o-hidden tr-200 border f-row-center-start"
-				key={uuidv4()}
-			>
-				<img className="w-100 h-100" src={item.image} alt={item.name} />
-				<div className="name f-col-center-end tr-300 ">
-					<a
-						className="view tr-200 mg-bottom-20 pd-20 pd-top-10 pd-bottom-10 border-none pointer bg-white br-10"
-						href={item.link}
-						title="Découvrir ce type de produit"
-					>
-						<Icon icon={["fas", "eye"]} /> Jetter un oeil
-					</a>
-					<div className="content bg-theme pointer-none">
-						<h3 className="fs-90 pd-10 tx-center white">{item.name}</h3>
-						<div className="separator w-30 h-5px bg-white br-5 mg-auto"></div>
-						<p className="about fs-80 white pd-10 tx-justify">{item.description}</p>
-					</div>
+const mappedType: Array<JSX.Element> = listTypes.map((item: ITypeMachineItem) => (
+		<li
+			className="item o-hidden tr-200 border f-row-center-start"
+			key={ uuidv4() }
+		>
+			<img className="w-100 h-100" src={ item.image } alt={ item.name } />
+			<div className="name f-col-center-end tr-300 ">
+				<a
+					className="view tr-200 mg-bottom-20 pd-20 pd-top-10 pd-bottom-10 border-none pointer bg-white br-10"
+					href={ item.link }
+					title="Découvrir ce type de produit"
+				>
+					<Icon icon={ ["fas", "eye"] } /> Jetter un oeil
+				</a>
+				<div className="content bg-theme pointer-none">
+					<h3 className="fs-90 pd-10 tx-center white">{ item.name }</h3>
+					<div className="separator w-30 h-5px bg-white br-5 mg-auto"></div>
+					<p className="about fs-80 white pd-10 tx-justify">{ item.description }</p>
 				</div>
-			</li>
-		)
-	);
-
-	return (
-		<section className="type-machine pt-40 pb-20">
-			<div className="ctn pd-top-40">
-				<div className="f-row-start-center pd-bottom-20">
-					<div className="bg-theme mg-right-15 w-5px h-40px br-5"></div>
-					<h2 className="fs-250">Quelle machine recherchez-vous ?</h2>
-				</div>
-				<p className="tx-justify">
-					Proin porta iaculis purus, et congue nibh placerat eu. Mauris interdum
-					lectus aliquet erat tincidunt suscipit ultricies eget purus. Nulla mattis
-					orci eget iaculis pharetra. Vestibulum mattis magna non volutpat malesuada.
-					Duis egestas nec sapien quis interdum. Duis placerat finibus vulputate. Nam
-					hendrerit erat turpis, ac condimentum massa commodo vitae. Lorem ipsum
-					dolor sit amet, consectetur adipiscing elit. Cras vel nibh quam.
-				</p>
-				<ul className="f-row-between-start f-wrap pd-top-20">{mappedType}</ul>
 			</div>
-		</section>
-	);
-};
+		</li>
+	)
+);
+
+const TypeMachine = (): JSX.Element => (
+	<section className="type-machine pt-40 pb-20">
+		<div className="ctn pd-top-40">
+			<div className="f-row-start-center pd-bottom-20">
+				<div className="bg-theme mg-right-15 w-5px h-40px br-5"></div>
+				<h2 className="fs-250">Quelle machine recherchez-vous ?</h2>
+			</div>
+			<p className="tx-justify">
+				Proin porta iaculis purus, et congue nibh placerat eu. Mauris interdum
+				lectus aliquet erat tincidunt suscipit ultricies eget purus. Nulla mattis
+				orci eget iaculis pharetra. Vestibulum mattis magna non volutpat malesuada.
+				Duis egestas nec sapien quis interdum. Duis placerat finibus vulputate. Nam
+				hendrerit erat turpis, ac condimentum massa commodo vitae. Lorem ipsum
+				dolor sit amet, consectetur adipiscing elit. Cras vel nibh quam.
+			</p>
+			<ul className="f-row-between-start f-wrap pd-top-20">{ mappedType }</ul>
+		</div>
+	</section>
+);
 
 export default TypeMachine;
