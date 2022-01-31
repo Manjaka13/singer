ef2566cb26857499a89e88f409d91fbcc748f274
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-01-31 20:25:11 +0100

    update icons list

--- helpers/icons.ts
+++ helpers/icons.ts
@@ -1,27 +1,8 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import {
-	faEnvelope,
-	faHeart,
-	faStar,
-	faShoppingCart,
-	faMapMarkerAlt,
-	faPhone,
-	faCalendar,
-	faTimesCircle,
-	faCheckCircle,
-	faCalendarAlt,
-	faUser,
-	faLock,
-	faWrench,
-	faBars,
-	faEye
-} from "@fortawesome/free-solid-svg-icons";
-import {
-	faFacebook,
-	faInstagram
-} from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 /*
 	Import all icons here
@@ -30,22 +11,4 @@ import {
 // Manually add fontawesome styles
 config.autoAddCss = false;
 
-library.add(
-	faFacebook,
-	faInstagram,
-	faEnvelope,
-	faHeart,
-	faStar,
-	faShoppingCart,
-	faMapMarkerAlt,
-	faPhone,
-	faCalendar,
-	faTimesCircle,
-	faCheckCircle,
-	faCalendarAlt,
-	faUser,
-	faLock,
-	faWrench,
-	faBars,
-	faEye
-);
+library.add(fas, fab);
